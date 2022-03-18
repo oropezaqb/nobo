651ec8eb410aecb24e81d1b5a9f99f52b4be659d
root <root@instance-1.us-west4-b.c.able-veld-340816.internal>
2022-03-18 09:18:46 +0000

    Added Payee BREAD

--- app/Models/Payee.php
+++ app/Models/Payee.php
@@ -11,6 +11,10 @@ class Payee extends Model
     protected $guarded = [];
     public function path()
     {
-        return route('payee.show', $this);
+        return route('payees.show', $this);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends \TCG\Voyager\Models\User
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function payees()
+    {
+        return $this->hasMany(Payee::class);
+    }
 }
