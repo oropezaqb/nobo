a554d1754409e93eb4c5873b26f92effa4ad9a87
root <root@instance-1.us-west4-b.c.able-veld-340816.internal>
2022-03-18 05:24:03 +0000

    Added Index for Payees

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
--- resources/views/payee/index.blade.php
+++ resources/views/payee/index.blade.php
@@ -1,54 +1,55 @@
-@extends ('layouts.app')
-@section ('content')
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header font-weight-bold">Supplier Credits</div>
-            <div class="card-body">
-                <div id="wrapper">
-                    <div
-                        id="page"
-                        class="container"
-                    >
-                        <h6 class="font-weight-bold">Search</h6>
-                        <form method="GET" action="/payee">
-                            @csrf
-                            <div class="form-group">
-                                <label for="payee_name">Payee Name: </label>
-                                <input 
-                                    class="form-control @error('payee_name') is-danger @enderror" 
-                                    type="text" 
-                                    name="payee_name" 
-                                    id="payee_name" required
-                                    value="{{ old('payee_name') }}">
-                                @error('payee_name')
-                                    <p class="help is-danger">{{ $message }}</p>
-                                @enderror
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __($header) }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+
+                    <h6 class="font-weight-bold">Search</h6>
+                    <form method="GET" action="/payee">
+                        @csrf
+                        <div class="form-group">
+                            <label for="payee_name">Payee Name: </label>
+                            <input 
+                                class="form-control @error('payee_name') is-danger @enderror" 
+                                type="text" 
+                                name="payee_name" 
+                                id="payee_name" required
+                                value="{{ old('payee_name') }}">
+                            @error('payee_name')
+                                <p class="help is-danger">{{ $message }}</p>
+                            @enderror
+                        </div>
+                        <button class="btn btn-outline-primary" type="submit">Search</button>
+                    </form>
+                    <br>
+                    <h6 class="font-weight-bold">Add</h6>
+                    <p>Want to record a new payee? Click <a class="text-primary" href="{{ url('/payee/create') }}">here</a>!</p>
+                    <br>
+                    <h6 class="font-weight-bold">List</h6>
+                    @forelse ($payees as $payee)
+                        <div id="content">
+                            <div id="title">
+                                <div style="display:inline-block;"><button class="btn btn-link" onclick="location.href = '{{ $payee->path() }}';">View</button></div>
+                                <div style="display:inline-block;"><button class="btn btn-link" onclick="location.href = '/payee/{{ $payee->id }}/edit';">Edit</button></div>
+                                <div style="display:inline-block;"><form method="POST" action="/payee/{{ $payee->id }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-link" type="submit">Delete</button>
+                                </form></div><div style="display:inline-block;">&nbsp;&nbsp;{{ $payee->name }}</div>
                             </div>
-                            <button class="btn btn-primary" type="submit">Search</button>
-                        </form>
-                        <p></p>
-                        <h6 class="font-weight-bold">Add</h6>
-                        <p>Want to record a new payee? Click <a href="{{ url('/payee/create') }}">here</a>!</p>
-                        <p></p>
-                        <h6 class="font-weight-bold">List</h6>
-                        @forelse ($payees as $payee)
-                            <div id="content">
-                                <div id="title">
-                                    <div style="display:inline-block;"><button class="btn btn-link" onclick="location.href = '{{ $payee->path() }}';">View</button></div>
-                                    <div style="display:inline-block;"><button class="btn btn-link" onclick="location.href = '/payee/{{ $payee->id }}/edit';">Edit</button></div>
-                                    <div style="display:inline-block;"><form method="POST" action="/payee/{{ $payee->id }}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="btn btn-link" type="submit">Delete</button>
-                                    </form></div><div style="display:inline-block;">&nbsp;&nbsp;{{ $payee->name }}</div>
-                                </div>
-                            </div>
-                        @empty
-                            <p>No payees recorded yet.</p>
-                        @endforelse
-                    </div>
+                        </div>
+                    @empty
+                        <p>No payees recorded yet.</p>
+                    @endforelse
+
                 </div>
             </div>
         </div>
     </div>
-@endsection
+</x-app-layout>
